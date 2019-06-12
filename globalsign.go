16d2d6dd1cd6566eddfd9f4a3b007ee6ee73fafc
hezhaowu <hezhaowu@kingsoft.com>
2019-06-12 22:39:39 +0800

    add DB interface

--- globalsign.go
+++ globalsign.go
@@ -528,6 +528,490 @@ func (gs *gsSession) RunCollection(c string, f CollectionFunc, args ...interface
 	return f(cl, args...)
 }
 
+
+func (gs *gsSession) DBCount(d string,c  string) (n int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).Count()
+}
+func (gs *gsSession) DBIndexes(d string,c string) (indexes []mgo.Index, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).Indexes()
+}
+func (gs *gsSession) DBEnsureIndex(d string,c string, index mgo.Index) (err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).EnsureIndex(index)
+}
+func (gs *gsSession) DBEnsureIndexKey(d string,c string, key ...string) (err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).EnsureIndexKey(key...)
+}
+func (gs *gsSession) DBFindOne(d string,c string, ret interface{}, query interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	err = cs.DB(d).C(c).Find(query).One(ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBFindAll(d string,c string, ret interface{}, query interface{}, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	return q.All(ret)
+}
+
+func (gs *gsSession) DBFindRange(d string,c string, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	if skip > 0 {
+		q.Skip(int(skip))
+	}
+	if limit > 0 {
+		q.Limit(int(limit))
+	}
+	return q.All(ret)
+}
+
+func (gs *gsSession) DBFindPage(d string,c string, tot *uint32, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query)
+	t, err := q.Count()
+	if err != nil {
+		return err
+	}
+	*tot = uint32(t)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	if skip > 0 {
+		q.Skip(int(skip))
+	}
+	if limit > 0 {
+		q.Limit(int(limit))
+	}
+	return q.All(ret)
+}
+
+func (gs *gsSession) DBFindDistinct(d string,c string, ret interface{}, query interface{}, key string, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	return q.Distinct(key, ret)
+}
+
+func (gs *gsSession) DBFindId(d string,c string, ret interface{}, id interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).FindId(id).One(ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+
+func (gs *gsSession) DBSelectOne(d string,c string, ret interface{}, query interface{}, projection interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	err = cs.DB(d).C(c).Find(query).Select(projection).One(ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBSelectAll(d string,c string, ret interface{}, query interface{}, projection interface{}, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query).Select(projection)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	return q.All(ret)
+}
+func (gs *gsSession) DBSelectRange(d string,c string, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query).Select(projection)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	if skip > 0 {
+		q.Skip(int(skip))
+	}
+	if limit > 0 {
+		q.Limit(int(limit))
+	}
+	return q.All(ret)
+}
+func (gs *gsSession) DBSelectPage(d string,c string, tot *uint32, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	if projection == nil {
+		projection = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query).Select(projection)
+	t, err := q.Count()
+	if err != nil {
+		return err
+	}
+	*tot = uint32(t)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	if skip > 0 {
+		q.Skip(int(skip))
+	}
+	if limit > 0 {
+		q.Limit(int(limit))
+	}
+	return q.All(ret)
+}
+func (gs *gsSession) DBSelectDistinct(d string,c string, ret interface{}, query interface{}, projection interface{}, key string, sort ...string) error {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	if projection == nil {
+		projection = EMPTY_QUERY
+	}
+	q := cs.DB(d).C(c).Find(query).Select(projection)
+	if len(sort) > 0 {
+		q.Sort(sort...)
+	}
+	return q.Distinct(key, ret)
+}
+func (gs *gsSession) DBSelectId(d string,c string, ret interface{}, id interface{}, projection interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).FindId(id).Select(projection).One(ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+
+func (gs *gsSession) DBFindAndUpdate(d string,c string, ret interface{}, query interface{}, update interface{}) (updated int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	ci, err := cs.DB(d).C(c).Find(query).Apply(mgo.Change{
+		Update:    update,
+		Upsert:    false,
+		Remove:    false,
+		ReturnNew: false,
+	}, ret)
+	if err != nil {
+		return
+	}
+	updated = ci.Updated
+	return
+}
+func (gs *gsSession) DBFindAndUpsert(d string,c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	ci, err := cs.DB(d).C(c).Find(query).Apply(mgo.Change{
+		Update:    upsert,
+		Upsert:    true,
+		Remove:    false,
+		ReturnNew: false,
+	}, ret)
+	if err != nil {
+		return
+	}
+	upsertedId = ci.UpsertedId
+	return
+}
+func (gs *gsSession) DBFindAndRemove(d string,c string, ret interface{}, query interface{}) (removed int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	ci, err := cs.DB(d).C(c).Find(query).Apply(mgo.Change{
+		Update:    nil,
+		Upsert:    false,
+		Remove:    true,
+		ReturnNew: false,
+	}, ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+		}
+		return
+	}
+	removed = ci.Removed
+	return
+}
+func (gs *gsSession) DBFindAndUpdateRN(d string,c string, ret interface{}, query interface{}, update interface{}) (updated int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	ci, err := cs.DB(d).C(c).Find(query).Apply(mgo.Change{
+		Update:    update,
+		Upsert:    false,
+		Remove:    false,
+		ReturnNew: true,
+	}, ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+		}
+		return
+	}
+	updated = ci.Updated
+	return
+}
+func (gs *gsSession) DBFindAndUpsertRN(d string,c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	if query == nil {
+		query = EMPTY_QUERY
+	}
+	ci, err := cs.DB(d).C(c).Find(query).Apply(mgo.Change{
+		Update:    upsert,
+		Upsert:    true,
+		Remove:    false,
+		ReturnNew: true,
+	}, ret)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+		}
+		return
+	}
+	upsertedId = ci.UpsertedId
+	return
+}
+
+func (gs *gsSession) DBInsert(d string,c string, docs ...interface{}) (err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).Insert(docs...)
+}
+func (gs *gsSession) DBRemoveOne(d string,c string, selector interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).Remove(selector)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBRemoveAll(d string,c string, selector interface{}) (removed int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	ci, err := cs.DB(d).C(c).RemoveAll(selector)
+	if err != nil {
+		return
+	}
+	removed = ci.Removed
+	return
+}
+func (gs *gsSession) DBRemoveId(d string,c string, id interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).RemoveId(id)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBUpdateOne(d string,c string, selector interface{}, update interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).Update(selector, update)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBUpdateAll(d string,c string, selector interface{}, update interface{}) (updated int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	ci, err := cs.DB(d).C(c).UpdateAll(selector, update)
+	if err != nil {
+		return
+	}
+	updated = ci.Updated
+	return
+}
+func (gs *gsSession) DBUpdateId(d string,c string, id interface{}, update interface{}) (ok bool, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	err = cs.DB(d).C(c).UpdateId(id, update)
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			err = nil
+			ok = false
+		} else {
+			return
+		}
+	} else {
+		ok = true
+	}
+	return
+}
+func (gs *gsSession) DBUpsertOne(d string,c string, selector interface{}, update interface{}) (upsertId interface{}, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).Upsert(selector, update)
+}
+func (gs *gsSession) DBUpsertId(d string,c string, id interface{}, update interface{}) (upsertId interface{}, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	return cs.DB(d).C(c).UpsertId(id, update)
+}
+func (gs *gsSession) DBRunBulk(d string,c string, f BulkFunc, args ...interface{}) (matched int, modified int, err error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	bk := cs.DB(d).C(c).Bulk()
+	f(&gsBulk{Bulk: bk}, args...)
+	rs, err := bk.Run()
+	if rs != nil && err == nil {
+		matched = rs.Matched
+		modified = rs.Modified
+	}
+	return
+}
+
+func (gs *gsSession) DBRunCollection(d string,c string, f CollectionFunc, args ...interface{}) (interface{}, error) {
+	cs := gs.Session.Copy()
+	defer cs.Close()
+
+	cl := cs.DB(d).C(c)
+	return f(cl, args...)
+}
+
 func (gs *gsSession) RunSession(f SessionFunc, args ...interface{}) (interface{}, error) {
 	cs := gs.Session.Copy()
 	defer cs.Close()
--- mongo.go
+++ mongo.go
@@ -56,6 +56,44 @@ type Mongo interface {
 	UpsertId(c string, id interface{}, update interface{}) (upsertedId interface{}, err error)
 	RunBulk(c string, f BulkFunc, args ...interface{}) (matched int, modified int, err error)
 	RunCollection(c string, f CollectionFunc, args ...interface{}) (interface{}, error)
+
+	DBCount(d string, c string) (n int, err error)
+	DBIndexes(d string, c string) (indexes []mgo.Index, err error)
+	DBEnsureIndex(d string, c string, index mgo.Index) error
+	DBEnsureIndexKey(d string, c string, key ...string) error
+	// For whole document
+	DBFindOne(d string, c string, ret interface{}, query interface{}) (bool, error)
+	DBFindAll(d string, c string, ret interface{}, query interface{}, sort ...string) error
+	DBFindRange(d string, c string, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error
+	DBFindPage(d string, c string, tot *uint32, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error
+	DBFindDistinct(d string, c string, ret interface{}, query interface{}, key string, sort ...string) error
+	DBFindId(d string, c string, ret interface{}, id interface{}) (bool, error)
+	// Find And Select
+	DBSelectOne(d string, c string, ret interface{}, query interface{}, projection interface{}) (bool, error)
+	DBSelectAll(d string, c string, ret interface{}, query interface{}, projection interface{}, sort ...string) error
+	DBSelectRange(d string, c string, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error
+	DBSelectPage(d string, c string, tot *uint32, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error
+	DBSelectDistinct(d string, c string, ret interface{}, query interface{}, projection interface{}, key string, sort ...string) error
+	DBSelectId(d string, c string, ret interface{}, id interface{}, projection interface{}) (bool, error)
+	// FindAndModify
+	DBFindAndUpdate(d string, c string, ret interface{}, query interface{}, update interface{}) (updated int, err error)              // return old doucument
+	DBFindAndUpsert(d string, c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error)   // return old doucument
+	DBFindAndRemove(d string, c string, ret interface{}, query interface{}) (removed int, err error)                                  // return old doucument
+	DBFindAndUpdateRN(d string, c string, ret interface{}, query interface{}, update interface{}) (updated int, err error)            // return new doucument
+	DBFindAndUpsertRN(d string, c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error) // return new doucument
+
+	DBInsert(d string, c string, docs ...interface{}) error
+	DBRemoveOne(d string, c string, selector interface{}) (bool, error)
+	DBRemoveAll(d string, c string, selector interface{}) (removed int, err error)
+	DBRemoveId(d string, c string, id interface{}) (bool, error)
+	DBUpdateOne(d string, c string, selector interface{}, update interface{}) (bool, error)
+	DBUpdateAll(d string, c string, selector interface{}, update interface{}) (updated int, err error)
+	DBUpdateId(d string, c string, id interface{}, update interface{}) (bool, error)
+	DBUpsertOne(d string, c string, selector interface{}, update interface{}) (upsertedId interface{}, err error)
+	DBUpsertId(d string, c string, id interface{}, update interface{}) (upsertedId interface{}, err error)
+	DBRunBulk(d string, c string, f BulkFunc, args ...interface{}) (matched int, modified int, err error)
+	DBRunCollection(d string, c string, f CollectionFunc, args ...interface{}) (interface{}, error)
+
 	RunSession(f SessionFunc, args ...interface{}) (interface{}, error)
 }
 
@@ -160,6 +198,109 @@ func RunBulk(c string, f BulkFunc, args ...interface{}) (matched int, modified i
 
 func RunCollection(c string, f CollectionFunc, args ...interface{}) (interface{}, error) {
 	return Default.RunCollection(c, f, args...)
+}
+
+func DBCount(d string, c string) (n int, err error) {
+	return Default.DBCount(d, c)
+}
+func DBIndexes(d string, c string) (indexes []mgo.Index, err error) {
+	return Default.DBIndexes(d, c)
+}
+func DBEnsureIndex(d string, c string, index mgo.Index) error {
+	return Default.DBEnsureIndex(d, c, index)
+}
+func DBEnsureIndexKey(d string, c string, key ...string) error {
+	return Default.DBEnsureIndexKey(d, c, key...)
+}
+func DBFindOne(d string, c string, ret interface{}, query interface{}) (bool, error) {
+	return Default.DBFindOne(d, c, query, ret)
+}
+func DBFindAll(d string, c string, ret interface{}, query interface{}, sort ...string) error {
+	return Default.DBFindAll(d, c, ret, query, sort...)
+}
+func DBFindRange(d string, c string, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error {
+	return Default.DBFindRange(d, c, ret, query, skip, limit, sort...)
+}
+
+func DBFindPage(d string, c string, tot *uint32, ret interface{}, query interface{}, skip uint32, limit uint32, sort ...string) error {
+	return Default.DBFindPage(d, c, tot, ret, query, skip, limit, sort...)
+}
+
+func DBFindDistinct(d string, c string, ret interface{}, query interface{}, key string, sort ...string) error {
+	return Default.DBFindDistinct(d, c, ret, query, key, sort...)
+}
+func DBFindId(d string, c string, ret interface{}, id interface{}) (bool, error) {
+	return Default.DBFindId(d, c, ret, id)
+}
+
+func DBSelectOne(d string, c string, ret interface{}, query interface{}, projection interface{}) (bool, error) {
+	return Default.DBSelectOne(d, c, ret, query, projection)
+}
+func DBSelectAll(d string, c string, ret interface{}, query interface{}, projection interface{}, sort ...string) error {
+	return Default.DBSelectAll(d, c, ret, query, projection, sort...)
+}
+func DBSelectRange(d string, c string, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error {
+	return Default.DBSelectRange(d, c, ret, query, projection, skip, limit, sort...)
+}
+func DBSelectPage(d string, c string, tot *uint32, ret interface{}, query interface{}, projection interface{}, skip uint32, limit uint32, sort ...string) error {
+	return Default.DBSelectPage(d, c, tot, ret, query, projection, skip, limit, sort...)
+}
+func DBSelectDistinct(d string, c string, ret interface{}, query interface{}, projection interface{}, key string, sort ...string) error {
+	return Default.DBSelectDistinct(d, c, ret, query, projection, key, sort...)
+}
+func DBSelectId(d string, c string, ret interface{}, id interface{}, projection interface{}) (bool, error) {
+	return Default.DBSelectId(d, c, ret, id, projection)
+}
+
+func DBFindAndUpdate(d string, c string, ret interface{}, query interface{}, update interface{}) (updated int, err error) {
+	return Default.DBFindAndUpdate(d, c, ret, query, update)
+}
+func DBFindAndUpsert(d string, c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error) {
+	return Default.DBFindAndUpsert(d, c, ret, query, upsert)
+}
+func DBFindAndRemove(d string, c string, ret interface{}, query interface{}) (removed int, err error) {
+	return Default.DBFindAndRemove(d, c, ret, query)
+}
+func DBFindAndUpdateRN(d string, c string, ret interface{}, query interface{}, update interface{}) (updated int, err error) {
+	return Default.DBFindAndUpdateRN(d, c, ret, query, update)
+}
+func DBFindAndUpsertRN(d string, c string, ret interface{}, query interface{}, upsert interface{}) (upsertedId interface{}, err error) {
+	return Default.DBFindAndUpsertRN(d, c, ret, query, upsert)
+}
+
+func DBInsert(d string, c string, docs ...interface{}) error {
+	return Default.DBInsert(d, c, docs...)
+}
+func DBRemoveOne(d string, c string, selector interface{}) (bool, error) {
+	return Default.DBRemoveOne(d, c, selector)
+}
+func DBRemoveAll(d string, c string, selector interface{}) (removed int, err error) {
+	return Default.DBRemoveAll(d, c, selector)
+}
+func DBRemoveId(d string, c string, id interface{}) (bool, error) {
+	return Default.DBRemoveId(d, c, id)
+}
+func DBUpdateOne(d string, c string, selector interface{}, update interface{}) (bool, error) {
+	return Default.DBUpdateOne(d, c, selector, update)
+}
+func DBUpdateAll(d string, c string, selector interface{}, update interface{}) (updated int, err error) {
+	return Default.DBUpdateAll(d, c, selector, update)
+}
+func DBUpdateId(d string, c string, id interface{}, update interface{}) (bool, error) {
+	return Default.DBUpdateId(d, c, id, update)
+}
+func DBUpsertOne(d string, c string, selector interface{}, update interface{}) (upsertedId interface{}, err error) {
+	return Default.DBUpsertOne(d, c, selector, update)
+}
+func DBUpsertId(d string, c string, id interface{}, update interface{}) (upsertedId interface{}, err error) {
+	return Default.DBUpsertId(d, c, id, update)
+}
+func DBRunBulk(d string, c string, f BulkFunc, args ...interface{}) (matched int, modified int, err error) {
+	return Default.DBRunBulk(d, c, f, args...)
+}
+
+func DBRunCollection(d string, c string, f CollectionFunc, args ...interface{}) (interface{}, error) {
+	return Default.DBRunCollection(d, c, f, args...)
 }
 
 func RunSession(c string, f SessionFunc, args ...interface{}) (interface{}, error) {
